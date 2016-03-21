cbf322313edbfbcf03d83dde1a24b74ad7c728c4
Dileep stanley George <dsg@mpro.italy.itroot.adnet>
2016-03-21 14:24:16 +0100

    phpunit test

---
+++ server/test/ScoreManagerTest.php
@@ -0,0 +1,65 @@
+<?php
+
+class ScoreControllerTest extends PHPUnit_Framework_TestCase
+{
+    // ...
+
+    public function testScoreController()
+    {
+        // Arrange
+        $SM = new Controller\scoreController();
+
+        // Act
+        $frames = Array ( 
+            [0] => Array ( "first" => 5, "second" => 5 ), 
+            [1] => Array ( "first" => 3, "second" => 0 ) ,
+            [2] => Array ( "first" => 0, "second" => 0 ) ,
+            [3] => Array ( "first" => 0, "second" => 0 ) ,
+            [4] => Array ( "first" => 0, "second" => 0 ) ,
+            [5] => Array ( "first" => 0, "second" => 0 ) ,
+            [6] => Array ( "first" => 0, "second" => 0 ) ,
+            [7] => Array ( "first" => 0, "second" => 0 ),
+            [8] => Array ( "first" => 0, "second" => 0 ),
+            [9] => Array ( "first" => 0, "second" => 0, [third] => 0 ) ) ;
+        
+
+        // Assert
+        $this->assertEquals(16, $a->getTotalScore($frames));
+
+        // Act
+        $frames = Array ( 
+            [0] => Array ( "first" => 10, "second" => 0 ) ,
+            [1] => Array ( "first" => 10, "second" => 0 ) ,
+            [2] => Array ( "first" => 10, "second" => 0 ) ,
+            [3] => Array ( "first" => 10, "second" => 0 ) ,
+            [4] => Array ( "first" => 10, "second" => 0 ) ,
+            [5] => Array ( "first" => 10, "second" => 0 ) ,
+            [6] => Array ( "first" => 10, "second" => 0 ) ,
+            [7] => Array ( "first" => 10, "second" => 0 ),
+            [8] => Array ( "first" => 10, "second" => 0 ),
+            [9] => Array ( "first" => 10, "second" => 10, [third] => 10 ) ) ;
+        
+
+        // Assert
+        $this->assertEquals(300, $a->getTotalScore($frames));
+
+         // Act
+        $frames = Array ( 
+            [0] => Array ( "first" => 0, "second" => 0 ) ,
+            [1] => Array ( "first" => 0, "second" => 0 ) ,
+            [2] => Array ( "first" => 0, "second" => 0 ) ,
+            [3] => Array ( "first" => 0, "second" => 0 ) ,
+            [4] => Array ( "first" => 0, "second" => 0 ) ,
+            [5] => Array ( "first" => 0, "second" => 0 ) ,
+            [6] => Array ( "first" => 0, "second" => 0 ) ,
+            [7] => Array ( "first" => 0, "second" => 0 ),
+            [8] => Array ( "first" => 0, "second" => 0 ),
+            [9] => Array ( "first" => 0, "second" => 0, "third" => 0 ) ) ;
+        
+
+        // Assert
+        $this->assertEquals(0, $a->getTotalScore($frames));
+    }
+
+    // ...
+}
